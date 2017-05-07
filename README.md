c6443282ab84cad63078e7af6d0b41a7a73ad4fe
Eve Worthington <worthington.ee@gmail.com>
2017-05-06 20:24:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+## A Nonny Function
+
+A Twitter bot that tweets poetry using markov strings generated from the works of various COOL POETS.
+
+Uses Node, twit, RiTa, Request, and the RhymeBrain API.
+
+Poets: Walt Whitman, T.S. Eliot, Rainer Maria Rilke, William Anderson, John Keats, Gerard Manley Hopkins, Emily Dickinson
+
+Title: Leaves of Grass
+Author: Walt Whitman
+https://www.gutenberg.org/ebooks/1322
+
+Title: Poems
+Author: T. S. [Thomas Stearns] Eliot
+https://www.gutenberg.org/ebooks/1567
+
+Title: Poems
+Author: Rainer Maria Rilke
+https://www.gutenberg.org/ebooks/38594
+
+Title: Poems
+Author: William Anderson
+https://www.gutenberg.org/ebooks/54505
+
+Title: Poems 1817
+Author: John Keats
+https://www.gutenberg.org/ebooks/8209
+
+Title: Poems of Gerard Manley Hopkins
+       Now First Published
+Author: Gerard Manley Hopkins
+https://www.gutenberg.org/ebooks/22403
+
+Title: Poems: Three Series, Complete
+Author: Emily Dickinson
+https://www.gutenberg.org/ebooks/12242
+
+
+All of the above from Project Gutenberg: www.gutenberg.org
+
+## Todo:
+
+* Not tweet if there's no rhyme found...
+* Refactor to make all the functions anonymous
+* If somebody tweets at it, make up a poem that rhymes with their name (twitter handle? display name?)
---
+++ index.js
@@ -0,0 +1,89 @@
+var Twit = require('twit');
+var fs = require('fs');
+var request= require('request');
+var rita = require('rita');
+
+var filename = 'combined.txt';
+var inputText = '';
+var builtTweet = '';
+
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  console.log('OK: ' + filename);
+  inputText = data;
+  generateFirstPartOfTweet();
+});
+
+var bot = new Twit({
+  consumer_key: process.env.NONNYFUNC_CONSUMER_KEY,
+  consumer_secret: process.env.NONNYFUNC_CONSUMER_SECRET,
+  access_token: process.env.NONNYFUNC_ACCESS_TOKEN,
+  access_token_secret: process.env.NONNYFUNC_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
+});
+
+function generateFirstPartOfTweet() {
+  var markov = new rita.RiMarkov(3);
+  markov.loadText(inputText);
+  var sentence = markov.generateSentences(1);
+  builtTweet = sentence;
+  var lastWord = sentence[0].split(' ');
+  lastWord = lastWord[lastWord.length -1];
+  lastWord = lastWord.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,'');
+  if (lastWord.length <= 2) {
+    console.log('word too short:', lastWord);
+    return;
+  } else {
+    getRhyme(lastWord);
+  }
+}
+
+function pickRandomNumber1to3() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function getRhyme(rhymeMe) {
+  request({ url:'http://rhymebrain.com/talk?function=getRhymes&word=' + rhymeMe }, function (error, response, body) {
+    if (error) {
+      console.log('Error: ' + error);
+    } else if (JSON.parse(response.body).length === 0){
+      console.log('no good rhyme words.');
+      return;
+    } else {
+      var result = JSON.parse(response.body);
+      var rhymeWord = result[0].word;
+      if (result.length > 2) {
+        rhymeWord = result[pickRandomNumber1to3()].word;
+      }
+
+      if (rhymeWord.length <= 2) {
+        console.log('word too short:', rhymeWord);
+        return;
+      } else {
+        generateSecondPartOfTweet(rhymeWord);
+      }
+    }
+  });
+}
+
+function generateSecondPartOfTweet(result) {
+  word = result + '.';
+  var markov = new rita.RiMarkov(3);
+  markov.loadText(inputText);
+  var sentence2 = markov.generateSentences(1);
+  sentence2 = sentence2[0].split(' ');
+  sentence2.pop();
+  sentence2.push(word);
+  sentence2 = sentence2.join(' ');
+  builtTweet = builtTweet + '\n' + sentence2;
+  bot.post('statuses/update', {
+    status: builtTweet
+  },
+  function(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(builtTweet + ' was tweeted');
+    }
+  });
+}
