4bae63cc1534ca8c3b0a91efe786b86aaa6e2f3d
Eve Worthington <worthington.ee@gmail.com>
2017-05-06 21:14:17 -0700

    scheduling

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ fs.readFile(filename, 'utf8', function(err, data) {
   if (err) throw err;
   console.log('OK: ' + filename);
   inputText = data;
+  generateFirstPartOfTweet();
 });
 
 var bot = new Twit({
@@ -21,74 +22,68 @@ var bot = new Twit({
   timeout_ms: 60 * 1000
 });
 
-function makeWholeTweet() {
-  function generateFirstPartOfTweet() {
-    var markov = new rita.RiMarkov(3);
-    markov.loadText(inputText);
-    var sentence = markov.generateSentences(1);
-    builtTweet = sentence;
-    var lastWord = sentence[0].split(' ');
-    lastWord = lastWord[lastWord.length -1];
-    lastWord = lastWord.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,'');
-    if (lastWord.length <= 2) {
-      console.log('word too short:', lastWord);
-      return;
-    } else {
-      getRhyme(lastWord);
-    }
-  }
-
-  function pickRandomNumber1to3() {
-    return Math.floor(Math.random() * 6) + 1;
-  }
-
-  function getRhyme(rhymeMe) {
-    request({ url:'http://rhymebrain.com/talk?function=getRhymes&word=' + rhymeMe }, function (error, response, body) {
-      if (error) {
-        console.log('Error: ' + error);
-      } else if (JSON.parse(response.body).length === 0){
-        console.log('no good rhyme words.');
-        return;
-      } else {
-        var result = JSON.parse(response.body);
-        var rhymeWord = result[0].word;
-        if (result.length > 2) {
-          rhymeWord = result[pickRandomNumber1to3()].word;
-        }
-
-        if (rhymeWord.length <= 2) {
-          console.log('word too short:', rhymeWord);
-          return;
-        } else {
-          generateSecondPartOfTweet(rhymeWord);
-        }
-      }
-    });
-  }
-
-  function generateSecondPartOfTweet(result) {
-    word = result + '.';
-    var markov = new rita.RiMarkov(3);
-    markov.loadText(inputText);
-    var sentence2 = markov.generateSentences(1);
-    sentence2 = sentence2[0].split(' ');
-    sentence2.pop();
-    sentence2.push(word);
-    sentence2 = sentence2.join(' ');
-    builtTweet = builtTweet + '\n' + sentence2;
-    bot.post('statuses/update', {
-      status: builtTweet
-    },
-    function(err, data, response) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(builtTweet + ' was tweeted');
-      }
-    });
+function generateFirstPartOfTweet() {
+  var markov = new rita.RiMarkov(3);
+  markov.loadText(inputText);
+  var sentence = markov.generateSentences(1);
+  builtTweet = sentence;
+  var lastWord = sentence[0].split(' ');
+  lastWord = lastWord[lastWord.length -1];
+  lastWord = lastWord.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,'');
+  if (lastWord.length <= 2) {
+    console.log('word too short:', lastWord);
+    return;
+  } else {
+    getRhyme(lastWord);
   }
 }
 
-setInterval(function() {
-  makeWholeTweet();
-}, 60*1000*5);
+function pickRandomNumber1to3() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function getRhyme(rhymeMe) {
+  request({ url:'http://rhymebrain.com/talk?function=getRhymes&word=' + rhymeMe }, function (error, response, body) {
+    if (error) {
+      console.log('Error: ' + error);
+    } else if (JSON.parse(response.body).length === 0){
+      console.log('no good rhyme words.');
+      return;
+    } else {
+      var result = JSON.parse(response.body);
+      var rhymeWord = result[0].word;
+      if (result.length > 2) {
+        rhymeWord = result[pickRandomNumber1to3()].word;
+      }
+
+      if (rhymeWord.length <= 2) {
+        console.log('word too short:', rhymeWord);
+        return;
+      } else {
+        generateSecondPartOfTweet(rhymeWord);
+      }
+    }
+  });
+}
+
+function generateSecondPartOfTweet(result) {
+  word = result + '.';
+  var markov = new rita.RiMarkov(3);
+  markov.loadText(inputText);
+  var sentence2 = markov.generateSentences(1);
+  sentence2 = sentence2[0].split(' ');
+  sentence2.pop();
+  sentence2.push(word);
+  sentence2 = sentence2.join(' ');
+  builtTweet = builtTweet + '\n' + sentence2;
+  bot.post('statuses/update', {
+    status: builtTweet
+  },
+  function(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(builtTweet + ' was tweeted');
+    }
+  });
+}
