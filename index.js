9a6760617bf4530e07b5b0bb19945a418b95a48c
Eve Worthington <worthington.ee@gmail.com>
2017-05-22 21:32:26 -0700

    using rita to rhyme instead of rhymebrain

--- index.js
+++ index.js
@@ -4,82 +4,55 @@ var request= require('request');
 var rita = require('rita');
 
 var filename = 'combined.txt';
-var inputText = '';
-var builtTweet = '';
-
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  console.log('OK: ' + filename);
-  inputText = data;
-  generateFirstPartOfTweet();
-});
-
-var bot = new Twit({
-  consumer_key: process.env.NONNYFUNC_CONSUMER_KEY,
-  consumer_secret: process.env.NONNYFUNC_CONSUMER_SECRET,
-  access_token: process.env.NONNYFUNC_ACCESS_TOKEN,
-  access_token_secret: process.env.NONNYFUNC_ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000
-});
+var textData = fs.readFileSync(filename, 'utf8');
+var lexicon = new rita.RiLexicon();
+generateFirstPartOfTweet();
 
 function generateFirstPartOfTweet() {
   var markov = new rita.RiMarkov(3);
-  markov.loadText(inputText);
-  var sentence = markov.generateSentences(1);
-  builtTweet = sentence;
-  var lastWord = sentence[0].split(' ');
-  lastWord = lastWord[lastWord.length -1];
-  lastWord = lastWord.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,'');
-  if (lastWord.length <= 2) {
-    console.log('word too short:', lastWord);
-  } else {
-    getRhyme(lastWord);
+  markov.loadText(textData);
+  var sentences = markov.generateSentences(2);
+  var sen1 = sentences[0].split(' ');
+  var sen2 = sentences[1].split(' ');
+  var sen1lastWord = sen1[sen1.length - 1];
+  var sen2lastWord = sen2[sen2.length - 1];
+  var matchFound = false;
+  sen1.pop();
+  sen2.pop();
+  sen1lastWord = rita.trimPunctuation(sen1lastWord);
+  sen2lastWord = rita.trimPunctuation(sen2lastWord);
+  sen1.push(sen1lastWord);
+  console.log(sen1lastWord, sen2lastWord);
+
+  var completions = markov.getCompletions(sen2);
+  for (i = 0; i < completions.length; i++) {
+    if (lexicon.isRhyme(sen1lastWord, completions[i])) {
+      sen2.push(completions[i]);
+      console.log('Match found!', sen1lastWord, completions[i]);
+      matchFound = true;
+
+      sen1 = sen1.join(' ');
+      sen2 = sen2.join(' ');
+      var tweet = sen1 + '\n' + sen2;
+      sendTheDangTweet(tweet);
+    }
+  }
+
+  if (matchFound === false) {
+    console.log('No match found. Trying again.');
+    generateFirstPartOfTweet();
   }
 }
 
-function pickRandomNumber1to3() {
-  return Math.floor(Math.random() * 3) + 1;
-}
-
-function getRhyme(rhymeMe) {
-  request({ url:'http://rhymebrain.com/talk?function=getRhymes&word=' + rhymeMe }, function (error, response, body) {
-    if (error) {
-      console.log('Error: ' + error);
-    } else if (JSON.parse(response.body).length === 0){
-      console.log('no good rhyme words.');
-    } else {
-      var result = JSON.parse(response.body);
-      var rhymeWord = result[0].word + '.';
-      if (result.length > 2) {
-        rhymeWord = result[pickRandomNumber1to3()].word + '.';
-      }
-
-      if (rhymeWord.length <= 2) {
-        console.log('word too short:', rhymeWord);
-      } else {
-        generateSecondPartOfTweet(rhymeWord);
-      }
-    }
-  });
-}
-
-function generateSecondPartOfTweet(word) {
-  var markov = new rita.RiMarkov(3);
-  markov.loadText(inputText);
-  var sentence2 = markov.generateSentences(1);
-  sentence2 = sentence2[0].split(' ');
-  sentence2.pop();
-  sentence2.push(word);
-  sentence2 = sentence2.join(' ');
-  builtTweet = builtTweet + '\n' + sentence2;
+function sendTheDangTweet(tweet) {
   bot.post('statuses/update', {
-    status: builtTweet
+    status: tweet
   },
   function(err, data, response) {
     if (err) {
       console.log(err);
     } else {
-      console.log(builtTweet + ' was tweeted');
+      console.log(tweet + ' was tweeted');
     }
   });
 }
